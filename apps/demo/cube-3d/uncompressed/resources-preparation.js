62a6e325939e86497088a99b4fb11e873b69233a
KSXGitHub <hvksmr1996@gmail.com>
2015-10-04 22:07:36 +0700

    update resources-preparations.js

--- apps/demo/cube-3d/uncompressed/resources-preparation.js
+++ apps/demo/cube-3d/uncompressed/resources-preparation.js
@@ -2,6 +2,7 @@
 Resources.prototype = Object.create(null);
 window.resources = new Resources(window, document, universe);
 
+// Resources::Resources(Window, Document, Universe, void);
 function Resources(window, document, universe, undefined) {
 	'use strict';
 	
@@ -9,15 +10,46 @@ function Resources(window, document, universe, undefined) {
 	
 	var resources = this;
 	
-	main(kjsclasses);
-	
-	function main(kjsclasses) {
-		implementation();
+	if (main()) {
+		resources._error = 1;
+		throw "Bad support browser.";
 	}
 	
+	// int main(void);
+	function main() {
+		implementation();
+		if(glContextCreation(kxmlclasses, kgraphclasses))
+			return 1;
+		return 0;
+	}
+	
+	// void implementation(void);
 	function implementation() {
 		Object.setPrototypeOf(Resources.prototype, universe);
 		Object.setPrototypeOf(Object.prototype, resources);
+	}
+	
+	// int glContextCreation(ClassCollection, ClassCollection)
+	function glContextCreation(kxmlclasses, kgraphclasses) {
+		var document_util = new kxmlclasses.DocumentUtil(document);
+		var canvas = resources.canvas = document_util.create({
+			type: document_util.ELEMENT,
+			parent: document.body,
+			before: null,
+			children: ["Opps, your browser didn't supported HTMLCanvasElement."]
+		});
+		var gl = resources.gl = canvas.getContext("webgl");
+		if (!gl) {
+			document.body.removeChild(canvas);
+			document_util.create({
+				type: document_util.ELEMENT,
+				parent: document.body,
+				before: null,
+				children: ["Your browser don't support WebGL or WebGL are disabled."]
+			});
+			return 1;
+		}
+		return 0;
 	}
 	
 }
