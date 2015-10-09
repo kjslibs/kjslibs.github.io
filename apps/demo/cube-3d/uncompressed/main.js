41e2a7a7a0e49e0dbed348025bb6f39c9a507439
KSXGitHub <hvksmr1996@gmail.com>
2015-10-09 09:35:52 +0700

    update main.js

--- apps/demo/cube-3d/uncompressed/main.js
+++ apps/demo/cube-3d/uncompressed/main.js
@@ -2,6 +2,23 @@
 main();
 
 function main() {
+	'use strict';
+	
+	var allglobjs = new AllGLObjs(__main__, onerror);
+	
+	function __main__(param) {
+		console.log("__main__ was called.");
+		console.log("param:", param);
+		console.log("this:", this);
+		console.log("__main__:", __main__);
+	}
+	
+	function onerror(param) {
+		console.warn("onerror was called.");
+		console.log("param:", param);
+		console.log("this:", this);
+		console.log("__main__:", __main__);
+	}
 	
 }
 
