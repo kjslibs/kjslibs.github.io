144b0b2027cbd78241faa3a513fa3fd341852329
KSXGitHub <hvksmr1996@gmail.com>
2015-10-10 15:22:03 +0700

    main.js --> __main__(): add color[], add color definitors

--- apps/demo/cube-3d/uncompressed/main.js
+++ apps/demo/cube-3d/uncompressed/main.js
@@ -1,7 +1,7 @@
 
-main(window);
+main(window, Float32Array);
 
-function main(window) {
+function main(window, Float32Array) {
 	'use strict';
 	
 	window.allglobjs = new AllGLObjs(__main__, onerror);
@@ -9,6 +9,7 @@ function main(window) {
 	function __main__(allglobjs) {
 		
 		// front = clockwise, cull counter-clockwise
+		// float vertices[3 * 2 * 6];
 		var vertices = new Float32Array([
 			
 			// LEFT FACE
@@ -84,6 +85,85 @@ function main(window) {
 			+1.0, -1.0, +1.0
 			
 		]);
+		
+		// define color per vertex (see vertices[] above)
+		// float color[vertices.length];
+		var color = new Float32Array(vertices.length);
+		var define_color = window.define_color = (function () {
+			DefineColor.prototype = {
+				RED: createColor(1.0, 0.0, 0.0),
+				GREEN: createColor(0.0, 1.0, 0.0),
+				BLUE: createColor(0.0, 0.0, 1.0),
+				mix: mixColor,
+				mixBright: mixColor.bind(function (a, b) {
+					return a + b;
+				}),
+				mixAvg: mixColor.bind(function (a, b) {
+					return .5 * (a + b);
+				}),
+				mixDark: mixColor.bind(function (a, b) {
+					return 1.0 - a - b;
+				})
+			};
+			return new DefineColor();
+			function DefineColor() {
+				var define_color = this;
+				var position = 0;
+				Object.setPrototypeOf(addColor3f, define_color);
+				Object.setPrototypeOf(addColor3fv, define_color);
+				Object.setPrototypeOf(pointer, define_color);
+				define_color.addColor3f = addColor3f;
+				define_color.pointer = pointer;
+				function addColor3f(red, green, blue) {
+					addElement(red)(green)(blue);
+					return addColor3f;
+				};
+				function addColor3fv(rgb) {
+					addColor3f(rgb[0], rgb[1], rgb[2]);
+					return addColor3fv;
+				}
+				function pointer(pos) {
+					if (typeof pos !== "number") {
+						return position;
+					}
+					position = pos;
+					return pointer;
+				};
+				function addElement(e) {
+					color[position] = e;
+					++position;
+					return addElement;
+				}
+			}
+			function createColor(red, green, blue) {
+				return new Float32Array([red, green, blue]);
+			}
+			function mixColor(calculate, rgb1, rgb2) {
+				var rgb = new Float32Array(3);
+				make(0);
+				make(1);
+				make(2);
+				return rgb;
+				function make(index) {
+					rgb[index] = calculate(rgb1[index], rgb2[index]);
+				}
+			}
+		})();
+		(function (RR, GG, BB, mixAvg) {
+			var GB = mixAvg(GG, BB);
+			var RB = mixAvg(RR, BB);
+			var RG = mixAvg(RR, GG);
+			define_color
+				.pointer(0)
+				.addColor3fv
+					(RR)(RR)(RR)(RR)(RR)(RR) // LEFT FACE
+					(GG)(GG)(GG)(GG)(GG)(GG) // BOTTOM FACE
+					(BB)(BB)(BB)(BB)(BB)(BB) // BACK FACE
+					(GB)(GB)(GB)(GB)(GB)(GB) // RIGHT FACE
+					(RB)(RB)(RB)(RB)(RB)(RB) // TOP FACE
+					(RG)(RG)(RG)(RG)(RG)(RG) // FRONT FACE
+			; // end commands sequence
+		})(define_color.RED, define_color.GREEN, define_color.BLUE, define_color.mixAvg);
 		
 	}
 	
--- apps/demo/cube-3d/uncompressed/shader.vert
+++ apps/demo/cube-3d/uncompressed/shader.vert
@@ -1,10 +1,14 @@
 
+// For calculating position per vertex
 attribute vec3 a_position;
 uniform vec4 u_rotation;
+
+// For calculating color per fragment shader's extrema
+attribute vec3 a_color;
 varying vec3 v_color;
 
 void main() {
-	v_color = a_position; // Just a startup. I'm now don't know what to do with v_color.
+	v_color = a_color;
 	gl_Position = vec4(a_position, 1.0) * u_rotation;
 }
 
