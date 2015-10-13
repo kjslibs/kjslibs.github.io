215f5dc9392a89179fae1571e26ec6be03331ce2
KSXGitHub <hvksmr1996@gmail.com>
2015-10-13 16:08:28 +0700

    the-begin-of-everything.js: rebuild Matrix

--- apps/demos/cube-3d/uncompressed/the-begin-of-everything.js
+++ apps/demos/cube-3d/uncompressed/the-begin-of-everything.js
@@ -71,116 +71,69 @@ function Universe(window, document, undefined) {
 			}
 		}
 		
-		universe.Matrix = Matrix;
-		var _mat_key_objectProperty = Symbol("Matrix::symbols::objectProperty");
-		var _mat_key_matrixElement = Symbol("Matrix::symbols::matrixElement");
-		function Matrix(rows, cols, base) {
-			var matrix = this;
-			matrix.rows = rows;
-			matrix.cols = cols;
-			matrix.base = base;
-			matrix.clone = clone;
-			matrix.sub = sub;
-			matrix[_mat_key_matrixElement] = createElementAccessor;
-			function clone() {
-				return new Matrix(rows, cols, base[_arr_key_clone]());
-			}
-			function sub(firstrow, firstcol, rows, cols) {
-				var parent = this;
-				var first = parent[_mat_key_matrixElement](firstrow, firstcol);
-				var proto = SubMatrix.prototype = {
-					parent: parent,
-					first: first,
-					__proto__: parent
-				};
-				proto._kJs_setConst(_mat_key_matrixElement, createElementAccessor);
-				Object.freeze(proto);
-				return new SubMatrix();
-				function SubMatrix() {
-					Object.freeze(this);
-				}
-				function createElementAccessor(rowId, colId) {
-					var baseaccessor = parent[_mat_key_matrixElement](rowId + firstrow, colId + firstcol);
-					ElementAccessor.prototype = {
-						rowId: rowId,
-						colId: colId,
-						__proto__: baseaccessor
-					};
-					return new ElementAccessor();
-					function ElementAccessor() {
-						Object.freeze(this);
-					}
-				}
-			}
-			function createElementAccessor(i, j) {
-				var baseaccessor = base[_mat_key_objectProperty](i * rows + j);
-				var accessor = {
-					matrix: matrix,
-					rowId: i,
-					colId: j,
-					get: baseaccessor.get.bind(baseaccessor),
-					set: baseaccessor.set.bind(baseaccessor)
-				};
-				Object.defineProperty(accessor, "value", accessor);
-				Object.freeze(accessor);
-				return accessor;
-			}
-		}
-		Matrix.prototype = new (function () {
-			var proto = this;
-			Object.setPrototypeOf(Matrix, proto);
-			proto.symbols = Object.freeze({
-				objectProperty: _mat_key_objectProperty,
-				matrixElement: _mat_key_matrixElement
-			});
-			proto.create = create;
-			proto.createSquare = createSquare;
-			proto.createIdentity = createIdentity;
-			proto.turnIdentity = turnIdentity;
-			proto = Object.create(proto);
-			return proto;
-			function create(rows, cols, base) {
-				return new Matrix(rows, cols, base || new Float64Array(rows * cols));
-			}
-			function createSquare(size, base) {
-				return create(size, size, base);
-			}
-			function createIdentity(size, base) {
-				var result = createSquare(size, base);
-				turnIdentity(result, size);
-				return result;
-			}
-			function turnIdentity(matrix, size) {
-				var base = matrix.base;
-				var i = 0, l = size * size;
-				while (i != l) {
-					base[_mat_key_objectProperty](i).set(1);
-					++i;
-					var j = size;
-					for ( ; j; --j) {
-						base[_mat_key_objectProperty](i).set(0);
-						++i;
-					}
-				}
-			}
-		})();
-		universe[_mat_key_objectProperty] = (function () {
-			var proto = PropertyAccessor.prototype = {
-				get: function () {
-					return this.object[this.name];
-				},
-				set: function (value) {
-					this.object[this.name] = value;
+		var Matrix = universe.Matrix = (function () {
+			/*
+				
+				Matrix is an array (iterable objecy) which store matrix column next to column
+				
+				    | a d g |
+				Mat | b e h | => Array [a, b, c, d, e, f, g, h, i]
+				    | c f i |
+				
+				Matrix's size will be determine when call matrix calculating function
+				
+			*/
+			var proto = {
+				createCalculatingContext: createCalculatingContext,
+				__proto__: {
+					add: add,
+					createIdentity: createIdentity,
+					scalarMultiply: scalarMultiply,
+					matrixMultiply: matrixMultiply
 				}
 			};
-			Object.defineProperty(proto, "value", proto);
-			return function (index) {
-				return new PropertyAccessor(this, index);
+			return Object.create(proto);
+			function createCalculatingContext() {
+				// continue from here...
 			}
-			function PropertyAccessor(object, name) {
-				this.object = object;
-				this.name = name;
-				Object.freeze(this);
+			function MatrixCalculatingContext() {
+				// continue from here...
+			}
+			function add(matR, startR, matA, startA, matB, startB, elements) {
+				var limR = startR + elements;
+				for ( ; startR != limR; ++startR, ++startA, ++startB) {
+					matR[startR] = matA[startA] + matB[startB];
+				}
+				return add;
+			}
+			function createIdentity(matR, startR, size) {
+				for (var i = size; ; --i) {
+					matR[startR] = 1;
+					++startR;
+					if (i == 1) {
+						break;
+					}
+					for (var j = size; j; --j) {
+						matR[startR] = 0;
+						++startR;
+					}
+				}
+				return createIdentity;
+			}
+			function scalarMultiply(matR, startR, matA, startA, elements, factor) {
+				var limR = startR + elements;
+				for ( ; startR != limR; ++startR, ++startA) {
+					matR[startR] = factor * matA[startA];
+				}
+				return scalarMultiply;
+			}
+			function matrixMultiply(matR, startR, matA, startA, matB, startB, rowsAR, colsARrowsBR, colsBR) {
+				for (var i = rowsAR; i; --i) {
+					for (var j = colsBR; j; --j) {
+						// continue from here...
+					}
+				}
+				return matrixMultiply;
 			}
 		})();
 		
