2f01b98b3a0d36fdf20afee233a0f044caa4f5f2
KSXGitHub <hvksmr1996@gmail.com>
2015-10-14 19:06:41 +0700

    the-begin-of-everything.js: Matrix: fix matrix assemblers

--- apps/demos/cube-3d/uncompressed/the-begin-of-everything.js
+++ apps/demos/cube-3d/uncompressed/the-begin-of-everything.js
@@ -191,7 +191,7 @@ function Universe(window, document, undefined) {
 			}
 			function assembleRows(matR, startR, matA, startA, rowsA, matB, startB, rowsB, colsABR) {
 				subMatrix(matR, startR, rowsA, colsABR, matA, startA, rowsA, colsABR, 0, 0);
-				subMatrix(matR, getArrayIndex(startR, rowsA - 1, colsABR, rowsB), rowsB, colsABR, matB, startB, rowsB, colsABR, 0, 0);
+				subMatrix(matR, getArrayIndex(startR, rowsA - 1, colsABR, rowsA), rowsB, colsABR, matB, startB, rowsB, colsABR, 0, 0);
 				return assembleRows;
 			}
 			function assembleCols(matR, startR, matA, startA, colsA, matB, startB, colsB, rowsABR) {
